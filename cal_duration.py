b7ae3b58d57cc39a0fac4b0f957a36abb1df3da3
argoncheng <argon.cheng@gmail.com>
2026-06-25 14:23:28 +0800

    增加一个参数可以指定分析的目录，没有参数就分析本目录。

--- cal_duration.py
+++ cal_duration.py
@@ -1,4 +1,10 @@
 #coding=utf-8
+"""
+统计当前目录或指定目录中所有视频文件的时长并输出总时长。
+
+如果未提供目录参数，则分析当前目录；如果提供目录参数，则分析指定目录。
+"""
+import argparse
 import os
 import subprocess
 import sys
@@ -7,7 +13,8 @@ import sys
 # Get video_file's duration in seconds from its ffmpeg output message
 # Return 0 if error
 def video_duration(video_file):
-    ffmpegcmd = 'c:\Program Files (x86)\FormatFactory\\ffmpeg.exe'
+    # ffmpegcmd = 'c:\\Program Files (x86)\\FormatFactory\\ffmpeg.exe'
+    ffmpegcmd = 'C:\\Users\\Administrator\\AppData\\Local\\Microsoft\\WinGet\\Packages\\Gyan.FFmpeg_Microsoft.Winget.Source_8wekyb3d8bbwe\\ffmpeg-8.1.1-full_build\\bin\\ffmpeg.exe'
     # I dont know how to set encoding when call popen(). Use temp file instead.
     temp_file = 'cal_duration.tmp'
     if len(video_file) == 0:
@@ -56,6 +63,10 @@ def video_duration(video_file):
     return total
 
 # Try to calculate total duration of all video files
+# 计算给定文件夹中所有视频文件的总时长。
+# 参数：#     file_dir (str): 包含视频文件的文件夹路径。
+# 返回：
+#     int: 所有视频文件的总时长(以秒为单位)。
 def cal_total_duration(file_dir):
     video_exts = [".mp4", ".mkv"]
     total_duration = 0
@@ -70,11 +81,28 @@ def cal_total_duration(file_dir):
     return total_duration
 
 def main():
-    video_sec = cal_total_duration(".")
-    hour = int(video_sec/3600)
-    min = int((video_sec - hour * 3600)/60)
-    sec = video_sec - hour * 3600 - min * 60
-    print("Total duration of all video files is %02d:%02d:%02d" %(hour, min, sec))
+    parser = argparse.ArgumentParser(
+        description='统计当前目录或指定目录中所有视频文件的时长，并输出每个文件时长与总时长。',
+        epilog='如果未提供目录参数，则分析当前目录；如果提供目录参数，则分析指定目录。'
+    )
+    parser.add_argument(
+        'directory',
+        nargs='?',
+        default='.',
+        help='要分析的视频文件目录，默认当前目录'
+    )
+    args = parser.parse_args()
+
+    target_dir = args.directory
+    if not os.path.isdir(target_dir):
+        print(f"目录不存在: {target_dir}")
+        sys.exit(1)
+
+    video_sec = cal_total_duration(target_dir)
+    hour = int(video_sec / 3600)
+    minute = int((video_sec - hour * 3600) / 60)
+    sec = video_sec - hour * 3600 - minute * 60
+    print("Total duration of all video files is %02d:%02d:%02d" % (hour, minute, sec))
 
 
 if __name__ == '__main__':
